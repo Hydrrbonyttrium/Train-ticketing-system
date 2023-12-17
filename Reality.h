77e6ef2871277129d42518db04b5927418e046a1
222090217@nau.edu.cn <222090217@stu.nau.edu.cn>
2023-12-17 19:36:58 +0800

    fest(Reality):"添加对应的存储数据结构

--- Reality.h
+++ Reality.h
@@ -1,9 +1,12 @@
  #include<Graph.h>
  #include<public1.h>
+
+
  typedef struct City
  {
    VNode Station;
- };
+ };//Graph
+
 
  typedef struct Ticket
  {
@@ -12,20 +15,29 @@
    City end;
    int price;
    string seat;
- };
+ };//Linklist
+
+
  typedef struct User
  {
     string account;
     string password;
+    string name;
+    string sex;
+    string id;
     Ticket* tickets; 
     int authority;
- };
+ };//Sqlist
+
+
  typedef struct Train
  {
    string train_type;//车次类型（高铁/动车）
    string train_set;//车组(复兴/和谐)
    string seat_type;
- };
+ };//Sqlist
+
+
  typedef struct TrainNunmber
  {
    string number;
@@ -34,4 +46,5 @@
    int ticket_number;
    int distance;
    int train_distance;
- };
+   Tree carriage;
+ };//Sqlist
