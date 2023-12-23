1e78c48049711118731518ae59fb48a0612961c0
222090217@nau.edu.cn <222090217@stu.nau.edu.cn>
2023-12-23 10:18:11 +0800

    fix

--- DataEntry.cpp
+++ DataEntry.cpp
@@ -1,12 +1,12 @@
 #include "DataEntry.h"
 
-void ReadUser(SqListUser *User)
+void ReadUser(SqListUser &User)
 {
     FILE* file =fopen("data\\User.csv","r");
      if (file == NULL) {
         fprintf(stderr, "无法打开文件\n");
         exit(OVERFLOW);
     }
-   while (fscanf(file, "%s,%s,%s,%s,%s,%s,%d\n",User->elem.account,User->elem.password,User->elem.name,User->elem.sex,User->elem.id,User->elem.tickets,&User->elem.authority) == 6) 
-   User++;
+   while (fscanf(file, "%s,%s,%s,%s,%s,%s,%d\n",User.elem->account,User.elem->password,User.elem->name,User.elem->sex,User.elem->id,User.elem->tickets,&User.elem->authority) == 6) 
+   User.elem++;
 }
