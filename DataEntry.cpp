d3b29d162987401c7c6deeeac886bfa8f237041f
222090217@nau.edu.cn <222090217@stu.nau.edu.cn>
2023-12-23 11:00:36 +0800

    aaa

--- DataEntry.cpp
+++ DataEntry.cpp
@@ -7,8 +7,7 @@ void ReadUser(SqListUser &User)
         fprintf(stderr, "无法打开文件\n");
         exit(OVERFLOW);
     }
-   while (fscanf(file, "%99[^,],%99[^,],%99[^,],%99[^,],%99[^,],%d\n",
-                  User.elem->account, User.elem->password, User.elem->name, User.elem->sex, User.elem->id, &User.elem->authority) == 6) {   
+   while (fscanf(file, "%99[^,],%99[^,],%99[^,],%99[^,],%99[^,],%d\n",User.elem->account, User.elem->password, User.elem->name, User.elem->sex, User.elem->id, &User.elem->authority) == 6) {   
     }
    
    fclose(file);
